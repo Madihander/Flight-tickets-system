f0bd863efe3825e68a7ab07914678cebe7fcf4b7
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-13 13:03:36 +0600

    Закончил 1/5 блока связанного с добавлением данных в таблицу

--- src/InsertCommand.java
+++ src/InsertCommand.java
@@ -1,6 +1,19 @@
-public class InsertCommand implements Command{
-    @Override
-    public void execute() {
+import Tables.DataBase;
+import CommonClasses.dbConnection;
+import Tables.Operations.Insert;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class InsertCommand implements Command{
+    DataBase dataBase;
+    Connection conn;
+    public InsertCommand(DataBase dataBase) throws SQLException{
+        this.dataBase = dataBase;
+        conn = dbConnection.getInstance().getConnection();
+    }
+    @Override
+    public void execute(String tableName) {
+        dataBase.insertIntoTable(conn,tableName);
     }
 }
