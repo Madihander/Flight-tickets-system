1537781d9977388e1e31dbed90be04b8ff9b2336
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-13 12:47:03 +0600

    Основной класс для работы с БД

---
+++ src/Tables/DataBase.java
@@ -0,0 +1,43 @@
+package Tables;
+import Tables.Operations.Insert;
+
+import java.sql.Connection;
+import java.sql.Statement;
+public class DataBase {
+
+    public void insertIntoTable(Connection conn, String tableName) {
+
+    }
+
+    public void readData(Connection conn, String tableName) {
+
+    }
+
+    public void searchById(Connection conn, String tableName) {
+
+    }
+
+    public void deleteById(Connection conn, String tableName) {
+    }
+
+    private String chooseTable(String operation, String tableName, Connection conn) {
+        switch (operation) {
+            case "insert":
+                switch (tableName) {
+                    case "airports":
+                        Insert.insertIntoAirports(conn);
+                        break;
+                    case "customers":
+                        break;
+                    case "passengers":
+                        break;
+                    case "flights":
+                        break;
+                    case "tickets":
+                        break;
+
+                }
+                break;
+        }
+    }
+}
