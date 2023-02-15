8a3a46587ca59714f4e8e6a627edb406fb3bdced
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-15 09:48:18 +0600

    Импортируем интерфейс, для того чтобы потом работать с классами, которые наследуются от него

--- src/Tables/DataBase.java
+++ src/Tables/DataBase.java
@@ -1,11 +1,11 @@
 package Tables;
 import Tables.Operations.Insert;
-
+import CommonClasses.СommonClass;
 import java.sql.Connection;
 import java.sql.Statement;
 public class DataBase {
-    public String insertIntoTable(Connection conn, String tableName) {
-        return chooseTable("insert",tableName,conn);
+    public String insertIntoTable(Connection conn, СommonClass someClass, String tableName) {
+        return chooseTable("insert",someClass,tableName,conn);
     }
 
     public void readData(Connection conn, String tableName) {
@@ -19,13 +19,13 @@ public class DataBase {
     public void deleteById(Connection conn, String tableName) {
     }
 
-    private String chooseTable(String operation, String tableName, Connection conn) {
+    private String chooseTable(String operation, СommonClass someClass, String tableName, Connection conn) {
         String result = null;
         switch (operation) {
             case "insert":
                 switch (tableName) {
                     case "airports":
-                        result = Insert.insertIntoAirports(conn);
+                        result = Insert.insertIntoAirports(conn,someClass);
                         break;
                     case "customers":
                         break;
