2d2a3374b31a20e9487b93c5b4ed2fc1c8b278a6
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-15 18:03:19 +0600

    Возможность вызвать поиск - есть

--- src/FindCommand.java
+++ src/FindCommand.java
@@ -1,2 +1,22 @@
-public class FindCommand {
+import CommonClasses.dbConnection;
+import CommonClasses.СommonClass;
+import Tables.DataBase;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FindCommand implements Command {
+    DataBase dataBase;
+    Connection conn;
+
+    public FindCommand(DataBase dataBase) throws SQLException {
+        this.dataBase = dataBase;
+        conn = dbConnection.getInstance().getConnection();
+    }
+
+    @Override
+    public ResultSet execute(СommonClass someClass, String tableName) {
+        return dataBase.findData(conn, someClass, tableName);
+    }
 }
