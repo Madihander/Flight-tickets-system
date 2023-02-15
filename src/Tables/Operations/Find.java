966852d7160c59a6f027ab13d239ec349631e918
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-15 18:05:05 +0600

    Запрос делается с помощью данных someQuery. 1. строка запроса, 2.- 4. это аргументы для запроса. Например name = Madi и password = 12356

--- src/Tables/Operations/Find.java
+++ src/Tables/Operations/Find.java
@@ -1,4 +1,26 @@
 package Tables.Operations;
+import CommonClasses.СommonClass;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Find {
+    public static ResultSet findData(Connection conn, СommonClass someQuery, String tableName) {
+        ResultSet result = null;
+        String var = someQuery.printInfo().get(0);
+        String var1 = someQuery.printInfo().get(1);
+        String var2 = someQuery.printInfo().get(2);
+        String var3 = someQuery.printInfo().get(3);
+        Statement stmt;
+        try {
+            String query = String.format(var, tableName,var1,var2,var3);
+            stmt = conn.createStatement();
+            result = stmt.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        return result;
+    }
 }
