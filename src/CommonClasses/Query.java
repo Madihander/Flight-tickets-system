b1022e47d3b6bdcee9a065b061e893adebf96ec9
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-15 18:02:42 +0600

    Чтобы была возможность выполнить запрос с определенными аргументами добавил массив.

--- src/CommonClasses/Query.java
+++ src/CommonClasses/Query.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 
 public class Query implements СommonClass{
     String query;
-    public Query(String str){
-        query = str;
+    ArrayList<String> queryData = new ArrayList<String>();
+    public Query(String str,ArrayList<String> queryData){
+        this.query = str;
+        this.queryData = queryData;
     }
 
     @Override
     public ArrayList<String> printInfo() {
         ArrayList<String> data = new ArrayList<String>();
         data.add(query);
+        data.add(queryData.get(0));
+        data.add(queryData.get(1));
+        data.add(queryData.get(2));
         return data;
     }
 }
