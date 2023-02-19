f6dd799995cb434f5a240140cbff4fb2671c2345
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-19 12:29:43 +0600

    дали душу QueryUpdate

--- src/CommonClasses/QueryUpdate.java
+++ src/CommonClasses/QueryUpdate.java
@@ -1,4 +1,24 @@
 package CommonClasses;
 
 public class QueryUpdate {
+    private final String query;
+    private final String column;
+    private final String updateData;
+
+    public QueryUpdate(String query,String column, String updateData) {
+        this.query = query;
+        this.column = column;
+        this.updateData = updateData
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+    public String getUpdateData() {
+        return updateData;
+    }
 }
