d8d420715bf7bd6213f2a23c3b7f438c7e17dd8c
Madihander <79453431+Madihander@users.noreply.github.com>
2023-02-19 12:40:24 +0600

    Без айдишника не понятно кого обновлять

--- src/CommonClasses/QueryUpdate.java
+++ src/CommonClasses/QueryUpdate.java
@@ -3,11 +3,13 @@ package CommonClasses;
 import java.util.ArrayList;
 
 public class QueryUpdate implements СommonClass {
+    private final int id;
     private final String query;
     private final String column;
     private final String updateData;
 
-    public QueryUpdate(String query,String column, String updateData) {
+    public QueryUpdate(int id, String query,String column, String updateData) {
+        this.id = id;
         this.query = query;
         this.column = column;
         this.updateData = updateData;
@@ -16,6 +18,7 @@ public class QueryUpdate implements СommonClass {
     @Override
     public ArrayList<String> printInfo() {
         ArrayList<String> data = new ArrayList<String>();
+        data.add(String.valueOf(id));
         data.add(query);
         data.add(column);
         data.add(updateData);
